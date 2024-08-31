d5b07791bfd4eca310a69cd75e75af126b9a3648
suraphelb <suraphelbirhane@gmail.com>
2024-08-31 10:59:01 +0300

    Initial Commit

---
+++ Food-Delivery-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food-Delivery-App
+//
+//  Created by Suraphel Birhane on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food-Delivery-App/Food_Delivery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Delivery_AppApp.swift
+//  Food-Delivery-App
+//
+//  Created by Suraphel Birhane on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Food_Delivery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
